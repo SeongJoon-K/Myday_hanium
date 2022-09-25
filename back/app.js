33f2df0b13f266fb125b21508d234cbab3982f61
SEONGJOON KIM <jobcho6320@naver.com>
2022-09-25 23:21:14 +0900

    Add: node server

--- back/app.js
+++ back/app.js
@@ -1,1 +1,8 @@
-// prac
+const htpp = require('http');
+
+const server = http.createServer();
+
+const httpRequestListener = function (request, response) { // (3)
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ message: "Hello World!" }));
+  };
